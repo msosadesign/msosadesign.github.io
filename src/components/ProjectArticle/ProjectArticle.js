1925c9729439f1bc59c4211b5a5465947dc661e7
Miguel Sosa <msosadesign@gmail.com>
2024-03-06 17:54:02 +0100

    Add SPA for GitHub Pages boilerplate

--- src/components/ProjectArticle/ProjectArticle.js
+++ src/components/ProjectArticle/ProjectArticle.js
@@ -2,17 +2,18 @@ import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import "./ProjectArticle.css"
 
-const ProjectArticle = ( props ) => {
+const ProjectArticle = (props) => {
   const [content, setContent] = useState("");
 
   useEffect(() => {
-    fetch(props.href) 
+    fetch(props.href)
       .then((res) => res.text())
       .then((text) => setContent(text));
   });
 
   return (
     <article className="project-article">
+      <base target="_blank" />
       <ReactMarkdown children={content} />
     </article>
   );

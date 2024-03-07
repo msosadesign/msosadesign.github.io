88a0dc878e2d7ab564dfa60224491890e77e8a45
Miguel Sosa <msosadesign@gmail.com>
2024-03-07 23:37:28 +0100

    Remove 404.html and update index.html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ProjectArticle from './components/ProjectArticle/ProjectArticle';
 import projects from "./projects.json"
 import Error404 from './pages/Error404';
@@ -11,20 +11,18 @@ let firstProject = projects[0].article;
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/projects' element={<Projects />}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />}>
           <Route path='/projects/' element={<ProjectArticle href={firstProject} />} />
 
-            {projects.map(project => (
-              <Route key={project.id} path={project.id} element={<ProjectArticle href={project.article} />} />
-            ))}
+          {projects.map(project => (
+            <Route key={project.id} path={project.id} element={<ProjectArticle href={project.article} />} />
+          ))}
 
-          </Route>
-          <Route path='*' element={<Error404 />} />
-        </Routes>
-      </BrowserRouter>
+        </Route>
+        <Route path='*' element={<Error404 />} />
+      </Routes>
 
     </div>
   );
